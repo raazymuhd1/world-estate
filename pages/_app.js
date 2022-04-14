c7eb2af8e0a93c2a133abf31c4fa2cc720486e32
mohammed raazy <muhammadrazi51@gmail.com>
2022-04-14 14:19:26 +0700

    fix nprogress module issue on _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Router from 'next/router'
-import NProgress from "nProgress"
+import NProgress from 'nprogress'
 import { ChakraProvider } from "@chakra-ui/react"
 
 import {Layout} from "../components"
