48bf68ec88bc1c811af42c73a3b5a99aee586694
mohammed raazy <muhammadrazi51@gmail.com>
2022-04-14 14:24:56 +0700

    changing app name on footer.js

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import { Box } from "@chakra-ui/react"
 const Footer = () => {
   return (
     <Box textAlign="center" p="5" color="gray.600" borderTop="1px" borderColor="gray.100">
-        2022 RealEstate, Inc.
+        2022 WorldEstate, Inc.
     </Box>
   )
 }
